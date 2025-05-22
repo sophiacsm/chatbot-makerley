188e64b69321d03e416c322020f11bbd0d43ec95
sophiacsm <sophiamello0824@gmail.com>
2025-05-22 13:16:35 -0300

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,13 +1,12 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-import tomllib
+import streamlit as st
 load_dotenv()
 
-with open("config.toml", "rb") as f:
-    config = tomllib.load(f)
+api_key = st.secrets["gemini"]["api_key"] if "gemini" in st.secrets else os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=config["gemini"]["api_key"])
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-2.0-flash')
 
 def ask_gemini(context, question):
